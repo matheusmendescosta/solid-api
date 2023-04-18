c7228ee80c7299bdb609dc051ee28cfe0625579c
matheus mendes <matheus_mendess@live.com>
2023-04-18 01:37:30 -0300

    ajuste na estrutura do projeto

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,7 @@
 import {FastifyRequest, FastifyReply} from 'fastify'
 import { z } from "zod"
-import { RegisterUseCase } from '@/env/service/register'
-import { PrismaUserRepository } from '@/env/repositories/prisma/prisma-user-repository'
+import { RegisterUseCase } from '@/service/register'
+import { PrismaUserRepository } from '@/repositories/prisma/prisma-user-repository'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerBodySchema = z.object({
